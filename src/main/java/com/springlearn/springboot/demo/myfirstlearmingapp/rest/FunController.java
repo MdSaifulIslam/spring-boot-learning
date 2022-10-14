258fa8efd9caace4ce66e3ef1a61196dafadbd83
MdSaifulIslam <mohammadsaifulcsepstu@gmail.com>
2022-10-14 17:05:39 +0600

    -custom props added

--- src/main/java/com/springlearn/springboot/demo/myfirstlearmingapp/rest/FunController.java
+++ src/main/java/com/springlearn/springboot/demo/myfirstlearmingapp/rest/FunController.java
@@ -2,16 +2,23 @@ package com.springlearn.springboot.demo.myfirstlearmingapp.rest;
 
 import java.time.LocalTime;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunController {
 	
+	@Value("${coach.name}")
+	private String coachName;
+	
+	@Value("${team.name}")
+	private String teamName;
+	
 	@GetMapping("/")
 	public String printHello() {
 
-		return "===> Time updated test:- " + LocalTime.now();
+		return "===> Time updated test:- " + LocalTime.now() + "  " + coachName + "  " + teamName;
 	}
 	
 	@GetMapping("/workout")
